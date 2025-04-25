117f1a5bbcabbc2e5211283e3f2d717395def0a7
deeptesh-rout <153610471+deeptesh-rout@users.noreply.github.com>
2025-04-25 09:41:03 +0530

    commited

--- Deeptesh Rout 2341018166/MARCH/10 March 2025/JavaQuestion6.java
+++ Deeptesh Rout 2341018166/MARCH/10 March 2025/JavaQuestion6.java
@@ -1,12 +1,9 @@
 
-
 public class JavaQuestion6 {
 
 	public static void main(String[] args) {
 		System.out.println("Java improves the facilities of "
 				+ "Generics and collections ");
-
-				
 
 	}
 
